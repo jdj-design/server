73ae8cd70680d4020ee5dec4f88dd94d12708ead
jdj-design <doc_jones12@yahoo.com>
2025-03-10 13:50:11 -0400

    working on it

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('node:http');
+
+const server = http.createServer((request, response) => {
+    let url = request.url.split('/');
+    if (url[1] === '' ) {
+      if (request.method === 'GET') {
+        response.end("Hello World");
+      }
+    }
+    response.statusCode = 404;
+    response.end();
+  });
+  server.listen(3000, "localhost", () => {
+    console.log(`Server running at http://localhost:3000/`);
+  });
