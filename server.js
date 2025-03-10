9ef026ddfd66c7f5ae7503be015c007a5295dc0e
jdj-design <doc_jones12@yahoo.com>
2025-03-10 14:03:09 -0400

    staged

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const http = require('node:http');
+const PORT =5500;
+const host = 'localhost';
 
 const server = http.createServer((request, response) => {
     let url = request.url.split('/');
     if (url[1] === '' ) {
       if (request.method === 'GET') {
-        response.end("Hello World");
+        response.end("Hello, Galvanized");
       }
     }
     response.statusCode = 404;
     response.end();
   });
-  server.listen(3000, "localhost", () => {
-    console.log(`Server running at http://localhost:3000/`);
+  server.listen(PORT, host, () => {
+    console.log(`Server running on ${host}: ${PORT}`);
   });
