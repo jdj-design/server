2adccdd4d1a2134c4543deb167831b22ab621191
jdj-design <doc_jones12@yahoo.com>
2025-03-11 11:29:11 -0400

    updated

--- secondServer.js
+++ secondServer.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+app.use(express.json())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/api', (req, res)=>{
+    res.status(200).send('Response from root route!')
+})
+
+app.post('/api', (req, res) =>{
+    console.log(req.body);
+    res.status(201).send('body recieved')
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
